6005f69080ee7d4e02851a4e667a7a5612739070
Gadeoli <gabriel.moraisoliveira1@gmail.com>
2020-05-30 20:27:57 -0300

    Post

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from 'react'
+import {View, Text} from 'react-native'
+
 import Header from './src/components/Header'
+import Post from './src/components/Post';
 
 const App: () => React$Node = () => {
     return (
         <>
             <Header/>
+            <Post image={require('./assets/imgs/fence.jpg')} />
         </>
     );
-};
+}
 
-export default App;
+export default App
