c80a1b938d07486777b38cb3aa6547f212a13c5a
Gadeoli <gabriel.moraisoliveira1@gmail.com>
2020-06-01 22:54:01 -0300

    Aula: Adicionando Usario ao Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,36 @@
 import React from 'react'
 import { View, StyleSheet, Text, Platform, Image } from 'react-native'
+import {connect} from 'react-redux'
+import {Gravatar} from 'react-native-gravatar'
 
 import icon from '../../assets/imgs/icon.png'
 
-const Header = () => {
-  return <View style={styles.container}>
-      <View style={styles.rowContainer}>
-        <Image source={icon} style={styles.image}/>
-        <Text style={styles.title}>Lambe Lambe</Text>
-      </View>
-  </View>
+const Header = (props) => {
+    const name = props.name || 'Anonymous'
+    const gravatar = props.email ? 
+                <Gravatar options={{email: props.email, secure: true}} style={styles.avatar} /> :
+                null
+
+    return <View style={styles.container}>
+        <View style={styles.rowContainer}>
+            <Image source={icon} style={styles.image}/>
+            <Text style={styles.title}>Lambe Lambe</Text>
+        </View>
+        <View style={styles.userContainer}>
+            <Text style={styles.user}>{name}</Text>
+            {gravatar}
+        </View>
+    </View>
 }
 
-export default Header
+const mapStateToProps = ({user}) => {
+    return {
+        email: user.email,
+        name: user.name
+    }
+}
+
+export default connect(mapStateToProps, null)(Header)
 
 const styles = StyleSheet.create({
     container: {
@@ -20,7 +38,9 @@ const styles = StyleSheet.create({
         padding: 10,
         borderBottomWidth: 1,
         borderColor: '#BBB',
-        width: '100%'
+        width: '100%',
+        flexDirection:'row',
+        justifyContent: 'space-between'
     },
     rowContainer: {
         flexDirection: 'row',
@@ -37,5 +57,19 @@ const styles = StyleSheet.create({
         fontFamily: 'shelter',
         height: 30,
         fontSize: 28
+    },
+    userContainer: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    user: {
+        fontSize: 10,
+        color: '#888'
+    },
+    avatar: {
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+        marginLeft: 10
     }
 })
