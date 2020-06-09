ddeb105680eb7b4fb8b0e65b564aea70bfaaad1c
Gadeoli <gabriel.moraisoliveira1@gmail.com>
2020-06-09 18:07:35 -0300

    Aula: Usando Firebase Storage #3

--- functions/index.js
+++ functions/index.js
@@ -14,6 +14,37 @@ const storage = new Storage({
 
 exports.uploadImage = functions.https.onRequest((request, response) => {
     cors(request, response, () => {
-        
+        try {
+            const tmpImage = '/tmp/imageToSave.jpg'
+            fs.writeFileSync(tmpImage, 
+            request.body.image, 'base64')
+
+            const bucket = storage.bucket('lambelambe-udemy.appspot.com', {
+                uploadType: 'media',
+                destination: `/posts/${id}.jpg`,
+                metadata: {
+                    metadata: {
+                        contentType: 'image/jpeg',
+                        firebaseStorageDownloadTokens: id
+                    }
+                }
+            }, (err, file) => {
+                if(err){
+                    console.log(err)
+                    return response.status(500).json({error: err})
+                }else{
+                    const fileName = encodeURIComponent(file.name)
+                    const imageUrl = 'https://firebasestorage.googleapis.com/v0/b/' + 
+                    bucket.name + '/o/' + fileName + '?alt=media&token=' + id
+
+                    return response.status(201).json({imageUrl})
+                }
+            })
+            const id = uuid()
+            bucket.upload(tmpImage)
+        } catch (err) {
+            console.log(err)
+            return response.status(500).json({error: err})
+        }
     })
 });
