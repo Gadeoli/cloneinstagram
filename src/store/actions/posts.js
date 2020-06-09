d0047e408a0327f7c4bce74874a5fd6feaa6d8be
Gadeoli <gabriel.moraisoliveira1@gmail.com>
2020-06-09 16:27:58 -0300

    Aula: Post no Firebase

--- src/store/actions/posts.js
+++ src/store/actions/posts.js
@@ -2,11 +2,13 @@ import {
     ADD_POST,
     ADD_COMMENT
 } from './actionTypes'
+import axios from 'axios'
 
 export const addPost = post => {
-    return {
-        type: ADD_POST,
-        payload: post
+    return dispatch => {
+        axios.post('/posts.json', { ...post })
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
     }
 }
 
