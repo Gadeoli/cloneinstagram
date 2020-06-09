0b9fc6c5c9b858e85b93cbcf03f374dddd3059c6
Gadeoli <gabriel.moraisoliveira1@gmail.com>
2020-06-09 16:43:22 -0300

    Aula: Usando Firebase Storage #2

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,19 @@
 const functions = require('firebase-functions');
+const cors = require('cors')({origin: true})
+const fs = require('fs')
+const uuid = require('uuid-v4')
+const {Storage} = require('@google-cloud/storage')
+const storage = new Storage({
+    projectId: 'lambelambe-udemy',
+    keyFilename: 'lambelambe-udemy-firebase-adminsdk.json'
+})
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-
+exports.uploadImage = functions.https.onRequest((request, response) => {
+    cors(request, response, () => {
+        
+    })
 });
